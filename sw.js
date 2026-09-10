bf61df9b5f40a5590f0e1816014c134e33d8dc2d
prvnnotes <PRAVEEN.SHARMA9820@GMAIL.COM>
2026-09-10 17:48:34 +0530

    Refactor service worker for caching and fetch handling

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = "bulbule-ka-khel-v4";
 
-const FILES = [
+const APP_SHELL = [
   "./",
   "./index.html",
   "./styles.css",
@@ -8,74 +8,64 @@ const FILES = [
   "./manifest.json"
 ];
 
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
 
-self.addEventListener(
-  "install",
-  event => {
-
-    event.waitUntil(
-
-      caches.open(CACHE_NAME)
-        .then(cache =>
-          cache.addAll(FILES)
-        )
-
-    );
-
-    self.skipWaiting();
-
-  }
-);
-
-
-self.addEventListener(
-  "activate",
-  event => {
-
-    event.waitUntil(
-
-      caches.keys()
-        .then(keys =>
-          Promise.all(
-            keys
-              .filter(
-                key =>
-                  key !== CACHE_NAME
-              )
-              .map(
-                key =>
-                  caches.delete(key)
-              )
-          )
-        )
-
-    );
-
-    self.clients.claim();
-
-  }
-);
-
-
-self.addEventListener(
-  "fetch",
-  event => {
-
-    event.respondWith(
-
-      caches.match(
-        event.request
-      )
-      .then(cached => {
-
-        return (
-          cached ||
-          fetch(event.request)
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => {
+        return Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
         );
-
       })
+      .then(() => self.clients.claim())
+  );
+});
 
-    );
+// Fetch
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
 
-  }
-);
+  const url = new URL(event.request.url);
+
+  // Only handle files from this game
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return fetch(event.request)
+          .then((response) => {
+            // Save successful responses to cache
+            if (response && response.status === 200) {
+              const responseClone = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, responseClone);
+                });
+            }
+
+            return response;
+          })
+          .catch(() => {
+            // If offline and page isn't cached
+            return caches.match("./index.html");
+          });
+      })
+  );
+});
