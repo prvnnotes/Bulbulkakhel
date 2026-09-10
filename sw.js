c72c70bf3dcffb51dbb3f56ea10a401ae38814c4
prvnnotes <PRAVEEN.SHARMA9820@GMAIL.COM>
2026-09-10 17:27:15 +0530

    Refactor service worker for caching strategy

--- sw.js
+++ sw.js
@@ -1,5 +1,81 @@
-const CACHE="bulbule-v1";
-const FILES=["./","./index.html","./styles.css","./game.js","./manifest.json"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = "bulbule-ka-khel-v4";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./game.js",
+  "./manifest.json"
+];
+
+
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(FILES)
+        )
+
+    );
+
+    self.skipWaiting();
+
+  }
+);
+
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches.keys()
+        .then(keys =>
+          Promise.all(
+            keys
+              .filter(
+                key =>
+                  key !== CACHE_NAME
+              )
+              .map(
+                key =>
+                  caches.delete(key)
+              )
+          )
+        )
+
+    );
+
+    self.clients.claim();
+
+  }
+);
+
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      )
+      .then(cached => {
+
+        return (
+          cached ||
+          fetch(event.request)
+        );
+
+      })
+
+    );
+
+  }
+);
